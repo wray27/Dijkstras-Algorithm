e84dd8f6f960493ddcb95e42f3648a791b14a75e
wray27 <aw16997@my.bristol.ac.uk>
2020-03-05 19:05:04 +0000

    sorted out package and classes

--- Source/Main.java
+++ Source/Main.java
@@ -1,5 +1,9 @@
+import GraphPack.*;
+
 public class Main {
   public static void main(String[] args){
-    System.out.println("This is my first program in java");
+    System.out.println("Input ");
   }
+
+
 }
